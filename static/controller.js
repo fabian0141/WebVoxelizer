8dc36a5f964da3fa9527b555cbe99c2a8d8c461c
Fabian Wigger <fabian0141@gmx.de>
2025-02-23 22:50:25 +0100

    better camera

--- static/controller.js
+++ static/controller.js
@@ -1,5 +1,6 @@
 import "./lib/mat4.js";
 import "./lib/vec3.js";
+import { normalize } from "./lib/vec3.js";
 import "./lib/vec4.js";
 
 const PI = 6.28318;
@@ -15,19 +16,40 @@ var prevMouse = { x: 0, y: 0 };
 var canvas;
 var eye = vec3.fromValues(0, 0, 5);
 var view = mat4.create();
-const upVector = vec3.fromValues(0, 1, 0);
-const center = vec3.fromValues(0, 0, 0);
+let up = vec3.fromValues(0, 1, 0);
+let right = vec3.fromValues(1, 0, 0);
+let center = vec3.fromValues(0, 0, 0);
+mat4.lookAt(view, eye, center, up);
+
 
 let deltaX = 0;
 let deltaY = 0;
+let speed = 0.03;
 
 // Function to update camera position based on spherical coordinates
 function rotatePosition() {
+    if (Math.abs(deltaX) < 0.0001 && Math.abs(deltaY) < 0.0001) {
+        console.log(deltaX, deltaY);
+        return view;
+    } 
+    deltaX *= speed;
+    deltaY *= -speed;
+
+    var newPos = vec3.clone(eye);
+    vec3.scaleAndAdd(newPos, newPos, right, deltaX);
+    vec3.scaleAndAdd(newPos, newPos, up, deltaY);
+    vec3.sub(newPos, newPos, center);
+    vec3.normalize(newPos, newPos);
+    vec3.scale(eye, newPos, radius);
+    let newRight = vec3.create();
+    vec3.cross(newRight, up, eye);
+    vec3.cross(up, eye, right);
+    normalize(up, up);
+    vec3.normalize(right, newRight);
+    console.log(eye[0], eye[1], eye[2], up[0], up[1], up[2], right[0], right[1], right[2]);
 
 
-
-
-    mat4.lookAt(view, eye, center, upVector);
+    mat4.lookAt(view, eye, center, up);
     deltaX = 0;
     deltaY = 0;
     return view;
